2420be52ba48cd0c30947107b371da61a5d84972
MicheleRai <rainonemichele1999@gmail.com>
2023-01-16 19:10:42 +0100

    fix db issue

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
+
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function home() {
+        return view('guest.home');
+    }
+
+    public function movies() {
+
+        $movies = Movie::all();
+
+        return view('guest.movies', compact('movies'));
+    }
 }
